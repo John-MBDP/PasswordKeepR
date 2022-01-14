e231d249ff2398024585c3a0f6109e2609a76399
John <johnartuz@hotmail.com>
2022-01-14 16:25:08 -0500

    Clean up code for final deployment.  Implement filter functionality

--- routes/users.js
+++ routes/users.js
@@ -8,83 +8,6 @@
 const express = require("express");
 const router = express.Router();
 
-// module.exports = (db) => {
-//   // get all organizations of single user in ascennding order by id
-//   router.get("/dashboards", (req, res) => {
-//     db.query(`SELECT * FROM organizations ORDER BY id ASC LIMIT 6;`)
-//       .then((data) => {
-//         const users = data.rows;
-//         res.json({ users });
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-
-//   // get single organization by id
-//   router.get("/dashboard/edit/:id", (req, res) => {
-//     const id = parseInt(req.params.id);
-//     return db
-//       .query(`SELECT * FROM organizations WHERE id = $1`, [id])
-//       .then((data) => {
-//         const users = data.rows[0];
-//         res.render("editOrganization", { users });
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-
-//   // updated data in an existing user
-//   router.post("/edit/:id", (req, res) => {
-//     const id = parseInt(req.params.id);
-//     const { name, domain, username, site_password } = req.body;
-//     db.query(
-//       `UPDATE organizations SET name = $1, domain = $2, username = $3, site_password = $4 WHERE id = $5 RETURNING *`,
-//       [name, domain, username, site_password, id]
-//     )
-//       .then((data) => {
-//         const users = data.rows[0];
-//         res.redirect("/dashboard");
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-
-//   // add organization and insert data into organization and credential table
-//   router.post("/dashboard", (req, res) => {
-//     const { name, domain } = req.body;
-//     const { username, site_password } = req.body;
-//     return db
-//       .query(
-//         `INSERT INTO organizations (name, domain, username, site_password) VALUES ($1, $2, $3, $4) RETURNING *`,
-//         [name, domain, username, site_password]
-//       )
-//       .then((data) => {
-//         const users = data.rows[0];
-//         res.json(users);
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-
-//   // Delete a user
-//   router.post("/dashboard/:id", (req, res) => {
-//     const id = parseInt(req.params.id);
-//     db.query(`DELETE FROM organizations WHERE id = $1`, [id])
-//       .then((data) => {
-//         const users = data.rows[0];
-//         res.redirect("/dashboard");
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-//   return router;
-// };
-
 module.exports = (db) => {
   // get all organizations of single user in ascennding order by id
   router.get("/dashboards", (req, res) => {
@@ -158,6 +81,20 @@ module.exports = (db) => {
         const users = data.rows;
         console.log(users);
         res.render("dashboard", { users });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.get("/dashboard/json/:category", (req, res) => {
+    const category = req.params.category;
+
+    return db
+      .query(`SELECT * FROM organizations WHERE category = $1`, [category])
+      .then((data) => {
+        const users = data.rows;
+        res.json({ users });
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
